70379e7cdaed3eeb7070bfc998f596764006fcfe
nandinisahu99 <115337833+nandinisahu99@users.noreply.github.com>
2024-04-05 13:12:03 +0530

    Add files via upload

--- Routes/Savescore.js
+++ Routes/Savescore.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
 // const jwtSecret = "ShivNandHarshTiyaShubhRuchAnja1$#"
 const dotenv = require("dotenv");
 dotenv.config();
